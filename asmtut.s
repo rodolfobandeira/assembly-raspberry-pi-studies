5f97dca29e6311b217fb22110aca90c1a170a928
rodolfobandeira <contato@rodolfobandeira.com>
2016-07-08 10:17:30 -0400

    Hello World

--- asmtut.s
+++ asmtut.s
@@ -1,10 +1,18 @@
-.text /* Instructions starts here */
-
 .global _start
 
 _start:
-    MOV R0, #65
-    MOV R7, #1
+    MOV R7, #4 /* Syscall 4 is for output to screen */
+    MOV R0, #1
+    MOV R2, #12 /* Lenth of the string */
+    LDR R1, =message
+    SWI 0 /* SWI = Software Interrupt */
 
-SWI 0 /* SWI = Software Interrupt */
+end:
+    MOV R7, #1
+    SWI 0
+
+.data
+message:
+    .ascii "Hello World \n"
+
 
